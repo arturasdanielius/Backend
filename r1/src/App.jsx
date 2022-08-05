023cdce91daa6118e8ecc45f1813bf4c9c228837
Artūras Danielius <arturas.danielius@gmail.com>
2022-08-05 10:46:58 +0300

    prideta stiliaus ir numeracija

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -7,6 +7,8 @@ function App() {
 
     const [jokes, setJokes] = useState([]);
 
+    //pasiimam is bibliotekos duomenis is visos data  pasiimam jokes (a.data.jokes)
+
     useEffect(() => {
         axios.get('https://v2.jokeapi.dev/joke/Programming?amount=10')
         .then(a => setJokes(a.data.jokes));
@@ -16,11 +18,11 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Jokes</h1>
-                <ul>
-                    {
-                        jokes.map(u => u.joke ?<li key={u.id}><span>{u.joke}</span></li> : <li key={u.id}><span>{u.setup}{u.delivery}</span></li>)
+                <ol>
+                    {//ismapinam pagal id priskiriant joke, jei true, jei false - setup arba delivery, nes objektuose yra tik joke arba setup su delivery kartu. Pridejau siek tiek stiliaus
+                        jokes.map(u => u.joke ?<li key={u.id}><span style={{color: 'lightblue', fontSize: '14px'}}>{u.joke}</span></li> : <li key={u.id}><span style={{color: 'yellow', fontSize: '10px'}}>{u.setup}{u.delivery}</span></li>)
                     }
-                </ul>
+                </ol>
 
             </header>
         </div>
